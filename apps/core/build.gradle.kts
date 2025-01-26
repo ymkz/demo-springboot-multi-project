dff46831bd3fff0885c26aca8d87a13d01d022cc
ymkz <ymkz@users.noreply.github.com>
2025-01-26 15:47:32 +0900

    chore: toolchainを利用

--- apps/core/build.gradle.kts
+++ apps/core/build.gradle.kts
@@ -8,8 +8,9 @@ group = "dev.ymkz"
 version = "0.0.1-SNAPSHOT"
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_21
-  targetCompatibility = JavaVersion.VERSION_21
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(21))
+  }
 }
 
 configurations {
